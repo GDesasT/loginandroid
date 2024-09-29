305f2a4440e9e1a1eeec034fb526c0c287721420
Juan Gerardo Alcantar <53959987+GDesasT@users.noreply.github.com>
2024-09-29 14:47:01 -0600

    first commit

---
+++ app/src/main/java/com/example/logingera/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.logingera
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.ui.AppBarConfiguration
+import com.example.logingera.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
+            )
+        )
+
+    }
+}
